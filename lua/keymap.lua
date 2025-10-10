7cb3690a52cc361b4b98291dc94285ebc3396e30
Viraat Maurya <singh.himmat.himmat83@gmail.com>
2025-10-10 23:42:42 +0530

    some more simple improvemnets

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,41 +1,24 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
-
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Telescope Keymapping
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 --Keymapping for formatting from nvim formatter.
 
-
-vim.keymap.set('n', '<C-f>', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
-
+vim.keymap.set("n", "<C-f>", ":lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
 
 -- for selecting all
-vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc ='select all' })
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "select all" })
 
 --Map Ctrl+h/j/k/l for motions in insert mode
 vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
@@ -43,18 +26,10 @@ vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
 
-
-
---Keybindings save file using C-s in both inser and nomal mode 
+--Keybindings save file using C-s in both inser and nomal mode
 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true }) -- Normal mode
 vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true }) -- Insert mode
 
-vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
-vim.keymap.set('v', '<C-x>', '"+d', { noremap = true, silent = true })
-vim.keymap.set({'n', 'v', 'i'}, '<C-v>', '"+p', { noremap = true, silent = true })
-
-
-
-
-
-
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
+vim.keymap.set("v", "<C-x>", '"+d', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<C-v>", '"+p', { noremap = true, silent = true })
--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -5,7 +5,7 @@ return {
 		cmd = { "ConformInfo" },
 		keys = {
 			{
-				"<leader>f",
+				"<C-f>",
 				function()
 					require("conform").format({ async = true, lsp_format = "fallback" })
 				end,
@@ -62,12 +62,12 @@ return {
 					-- `friendly-snippets` contains a variety of premade snippets.
 					--    See the README about individual language/framework/plugin snippets:
 					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip.loaders.from_vscode").lazy_load()
+						end,
+					},
 				},
 				opts = {},
 			},
@@ -77,29 +77,12 @@ return {
 		--- @type blink.cmp.Config
 		opts = {
 			keymap = {
-				-- 'default' (recommended) for mappings similar to built-in completions
-				--   <c-y> to accept ([y]es) the completion.
-				--    This will auto-import if your LSP supports it.
-				--    This will expand snippets if the LSP sent a snippet.
-				-- 'super-tab' for tab to accept
-				-- 'enter' for enter to accept
-				-- 'none' for no mappings
-				--
-				-- For an understanding of why the 'default' preset is recommended,
-				-- you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
-				--
-				-- All presets have the following mappings:
-				-- <tab>/<s-tab>: move to right/left of your snippet expansion
-				-- <c-space>: Open menu or open docs if already open
-				-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-				-- <c-e>: Hide menu
-				-- <c-k>: Toggle signature help
-				--
-				-- See :h blink-cmp-config-keymap for defining your own keymap
-				preset = "default",
-
+				preset = "none", -- we'll define our own
+				["<CR>"] = { "accept", "fallback" }, -- confirm selection
+				["<Tab>"] = { "select_next", "fallback" }, -- next suggestion
+				["<S-Tab>"] = { "select_prev", "fallback" }, -- previous suggestion
+				["<C-e>"] = { "hide" }, -- close menu
+				["<C-Space>"] = { "show" },
 				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			},
