a8ebf4e4d3e6b80b538f20fbc73a48c494c71422
Viraat Maurya <singh.himmat.himmat83@gmail.com>
2025-10-10 23:10:57 +0530

    Minimalistic not finished but again working.

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
+}
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,10 +1,1 @@
--- General Neovim Settings
-vim.o.relativenumber = true -- Enable relative line numbers
-vim.o.expandtab = true -- Use spaces instead of tabs
-vim.o.shiftwidth = 2 -- Indent size
-vim.o.tabstop = 2 -- Tab size
--- Disable the default mode indicator
-vim.o.showmode = false
 
--- Optional: Set the command line height to 1
-vim.o.cmdheight = 1
