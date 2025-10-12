cfdf335f797906ea0a6b1eb8809194bfa63a01d0
Viraat Maurya <singh.himmat.himmat83@gmail.com>
2025-10-12 22:30:05 +0530

    Mostly done with config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,37 +15,9 @@ return {
 			},
 		})
 
-		-- Create a terminal dedicated for compilation
-		local Terminal = require("toggleterm.terminal").Terminal
-		local compile_term = Terminal:new({
-			cmd = "", -- will be set dynamically
-			hidden = true,
-			direction = "float",
-			close_on_exit = false,
-			on_open = function(term)
-				vim.api.nvim_buf_set_option(term.bufnr, "filetype", "compilelog")
-			end,
-		})
-
-		-- Function to compile and show status
-		function _G.compile_command(cmd)
-			if not cmd or cmd == "" then
-				cmd = vim.fn.input(":", "", "shellcmd")
-			end
-			local start_time = vim.loop.hrtime()
-			compile_term.cmd = cmd
-			compile_term:toggle()
-			compile_term:send(cmd .. "\n")
-		end
-
-		-- Keybindings
-		vim.keymap.set({ "n", "t" }, "<C-;>", function()
-			compile_command()
-		end, { desc = "Run compilation command in floating terminal" })
-
 		vim.keymap.set(
 			{ "n", "t" },
-			"<C-/>",
+			"<C-;>",
 			"<cmd>ToggleTerm direction=float<CR>",
 			{ desc = "Toggle floating terminal" }
 		)
